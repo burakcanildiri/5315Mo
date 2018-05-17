30de269b4c414061d87340e0a74ec9dc210cf37c
burak <ildirib@seattleu.edu>
2018-05-17 15:52:03 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -1,124 +1,52 @@
 {
   "data": [
     {
-      "name": "James Bond",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
-      "office": "Tokyo",
+      "name": "Michael Jordan",
+      "position": "Shooting Guard",
+      "salary": "$30,000,000",
+      "draft_date": "1984/06/19",
+      "city": "Chicago",
       "extension": "8422"
     },
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
+      "name": "LeBron James",
+      "position": "Small Forward",
+      "salary": "$25,000,000",
+      "draft_date": "2003/06/26",
+      "city": "Cleveland",
       "extension": "4804"
     },
     {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
+      "name": "Stephen Curry",
+      "position": "Point Guard",
+      "salary": "$23,000,000",
+      "draft_date": "2009/06/25",
+      "city": "San Francisco",
       "extension": "1562"
     },
     {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
-      "office": "Tokyo",
+      "name": "James Harden",
+      "position": "Point Guard",
+      "salary": "$18,000,000",
+      "draft_date": "2009/06/25",
+      "city": "Oklahoma City",
       "extension": "5407"
     },
     {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
+      "name": "Shaquille O'Neal",
+      "position": "Center",
+      "salary": "$27,000,000",
+      "draft_date": "1992/06/24",
+      "city": "Orlando",
       "extension": "8330"
     },
     {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
+      "name": "Kobe Bryant",
+      "position": "Shooting Guard",
+      "salary": "$28,000,000",
+      "draft_date": "1996/06/26",
+      "city": "Los Angeles",
       "extension": "6373"
     },
-    {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
-    },
-    {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
-    },
-    {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
-    },
-    {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
-    },
-    {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
-    },
-    {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
-      "office": "Tokyo",
-      "extension": "6200"
-    },
-    {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
-    },
-    {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
-    },
-    {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
-    }
   ]
 }
