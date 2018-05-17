9f2a8abc3ef4181f3af602a479748dfa49d85d71
burak <ildirib@seattleu.edu>
2018-05-17 15:55:43 -0700

    hw files

--- empdata.js
+++ empdata.js
@@ -2,50 +2,50 @@
   "data": [
     {
       "name": "Michael Jordan",
-      "position": "Shooting Guard",
+      "designation": "Shooting Guard",
       "salary": "$30,000,000",
-      "draft_date": "1984/06/19",
-      "city": "Chicago",
+      "joining_date": "1984/06/19",
+      "office": "Chicago",
       "extension": "8422"
     },
     {
       "name": "LeBron James",
-      "position": "Small Forward",
+      "designation": "Small Forward",
       "salary": "$25,000,000",
-      "draft_date": "2003/06/26",
-      "city": "Cleveland",
+      "joining_date": "2003/06/26",
+      "office": "Cleveland",
       "extension": "4804"
     },
     {
       "name": "Stephen Curry",
-      "position": "Point Guard",
+      "designation": "Point Guard",
       "salary": "$23,000,000",
-      "draft_date": "2009/06/25",
-      "city": "San Francisco",
+      "joining_date": "2009/06/25",
+      "office": "San Francisco",
       "extension": "1562"
     },
     {
       "name": "James Harden",
-      "position": "Point Guard",
+      "designation": "Point Guard",
       "salary": "$18,000,000",
-      "draft_date": "2009/06/25",
-      "city": "Oklahoma City",
+      "joining_date": "2009/06/25",
+      "office": "Oklahoma City",
       "extension": "5407"
     },
     {
       "name": "Shaquille O'Neal",
-      "position": "Center",
+      "designation": "Center",
       "salary": "$27,000,000",
-      "draft_date": "1992/06/24",
-      "city": "Orlando",
+      "joining_date": "1992/06/24",
+      "office": "Orlando",
       "extension": "8330"
     },
     {
       "name": "Kobe Bryant",
-      "position": "Shooting Guard",
+      "designation": "Shooting Guard",
       "salary": "$28,000,000",
-      "draft_date": "1996/06/26",
-      "city": "Los Angeles",
+      "joining_dates": "1996/06/26",
+      "office": "Los Angeles",
       "extension": "6373"
     },
   ]
